3512e5c8c5392e9a67af304202585b29e356e2d7
Clement Lacau <lacau@insa-toulouse.fr>
2025-02-25 16:49:48 +0100

    add: testing script

---
+++ kompilator/run_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ ! -d "sample_files" ]; then
+    echo "Error: sample_files directory not found!"
+    exit 1
+fi
+
+
+for FILE in sample_files/*; do
+    #echo "Running make exec FILE=$FILE..."
+    
+    OUTPUT=$(make exec FILE="$FILE" 2>&1)
+
+    if echo "$OUTPUT" | grep -q "Parsing successful! ✅"; then
+        echo "$FILE : SUCCESS ✅"
+    else
+        echo "$FILE : ERROR ❌"
+        #echo "----- OUTPUT -----"
+        #echo "$OUTPUT"
+        #echo "------------------"
+    fi
+done
+
