b0a8bf65da0d438638eda5ca5603a21fb7b15765
Clement Lacau <lacau@insa-toulouse.fr>
2025-02-25 16:37:08 +0100

    refactor: another sample file

--- kompilator/sample_files/another.c
+++ kompilator/sample_files/another.c
@@ -1,9 +1,12 @@
-main()
-{ int i, j,k ,r;
-i =3 ;
-j=4 ;
-k=8 ;
-printf (i) ;
-r=(i+j)*(i+k/j) ;
-printf ( r ) ;
+#include <stdio.h>
+
+void main ()
+{
+  int i, j, k, r;
+  i = 3;
+  j = 4;
+  k = 8;
+  printf("%d\n", i);
+  r = (i + j) * (i + k / j);
+  printf("%d\n",r);
 }
