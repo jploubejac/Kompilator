cf174109f28dd060087c372c8300fb64ad311363
Clement Lacau <lacau@insa-toulouse.fr>
2025-03-07 16:12:27 +0100

    refactor: file naming and add if file

---
+++ kompilator/sample_files/ex3.c
@@ -0,0 +1,18 @@
+void main ()
+{
+  int a = 3;
+  int b = 5;
+  if (a < b) {} else {}
+
+  if (a < b) {}
+
+  if (a > b) {} else {}
+
+  if (a == b || a < b) {} else {}
+
+  if (a == b && a < b) {} else {}
+
+  if (!(a == b)) {} else {}
+
+  if (a < b) {if (a < b) {} else {}} else {}
+}
