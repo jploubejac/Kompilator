1fbbe961a4ee2e9a0db56ab5584e775fa8b95f52
Clement Lacau <lacau@insa-toulouse.fr>
2025-05-08 01:21:42 +0200

    add: chemin afc, to be tested

--- u_controleur/ALUME/ALUME.srcs/sources_1/new/pipeline.vhd
+++ u_controleur/ALUME/ALUME.srcs/sources_1/new/pipeline.vhd
@@ -41,17 +41,6 @@ architecture Structural of pipeline is
        CLK : in STD_LOGIC;
        OUTD : out STD_LOGIC_VECTOR (31 downto 0));
     END COMPONENT;
-    COMPONENT etage_pipeline
-    PORT( A_i : in STD_LOGIC_VECTOR (7 downto 0);
-       OP_i : in STD_LOGIC_VECTOR (7 downto 0);
-       B_i : in STD_LOGIC_VECTOR (7 downto 0);
-       C_i : in STD_LOGIC_VECTOR (7 downto 0);
-       CLK : in STD_LOGIC; 
-       A_o : out STD_LOGIC_VECTOR (7 downto 0);
-       OP_o : out STD_LOGIC_VECTOR (7 downto 0);
-       B_o : out STD_LOGIC_VECTOR (7 downto 0);
-       C_o : out STD_LOGIC_VECTOR (7 downto 0));
-    END COMPONENT;
     COMPONENT banc_donnees
     PORT( ADDR : in STD_LOGIC_VECTOR (7 downto 0);
        IND : in STD_LOGIC_VECTOR (7 downto 0);
@@ -99,6 +88,7 @@ architecture Structural of pipeline is
     
     --===========================DI/EX===========================
     --Inputs
+    signal DIEX_A_i : std_logic_vector(7 downto 0) := (others => '0');
     signal DIEX_B_i : std_logic_vector(7 downto 0) := (others => '0');
     signal DIEX_C_i : std_logic_vector(7 downto 0) := (others => '0');
     signal DIEX_OP_i : std_logic_vector(7 downto 0) := (others => '0');
@@ -111,6 +101,7 @@ architecture Structural of pipeline is
     
     --===========================EX/MEM===========================
     --Inputs
+    signal EXMEM_A_i : std_logic_vector(7 downto 0) := (others => '0');
     signal EXMEM_B_i : std_logic_vector(7 downto 0) := (others => '0');
     signal EXMEM_C_i : std_logic_vector(7 downto 0) := (others => '0');
     signal EXMEM_OP_i : std_logic_vector(7 downto 0) := (others => '0');
@@ -123,6 +114,7 @@ architecture Structural of pipeline is
     
     --===========================MEM/RE===========================
     --Inputs
+    signal MEMRE_A_i : std_logic_vector(7 downto 0) := (others => '0');
     signal MEMRE_B_i : std_logic_vector(7 downto 0) := (others => '0');
     signal MEMRE_C_i : std_logic_vector(7 downto 0) := (others => '0');
     signal MEMRE_OP_i : std_logic_vector(7 downto 0) := (others => '0');
@@ -135,73 +127,119 @@ architecture Structural of pipeline is
     
     --===========================BANC_INSTRUCTION=========================== 
     --Inputs
-    signal BAIN_ADDR_i: std_logic_vector(7 downto 0) := (others => '0');     
+    signal BI_ADDR_i: std_logic_vector(7 downto 0) := (others => '0');
     
+    --Outputs     
+    signal BI_OUTD_o: std_logic_vector(32 downto 0) := (others => '0');     
+
+    --===========================BANC_DONNEES=========================== 
+    --Inputs
+    signal BD_ADDR_i : std_logic_vector(7 downto 0) := (others => '0');
+    signal BD_IND_i  : std_logic_vector(7 downto 0) := (others => '0');
+    signal BD_RW_i   : std_logic := '0';
+    signal BD_RST_i  : std_logic := '0';
+
+    --Outputs
+    signal BD_OUTD_o : std_logic_vector(7 downto 0) := (others => '0');
+
     --===========================BANC_REGISTREs=========================== 
+    --Inputs
+    signal BR_A_i: std_logic_vector(3 downto 0) := (others => '0');     
+    signal BR_B_i: std_logic_vector(3 downto 0) := (others => '0');     
+    signal BR_W_i: std_logic_vector(3 downto 0) := (others => '0');     
+    signal BR_Wb_i: std_logic := '0';
+    signal BR_DATA_i: std_logic_vector(7 downto 0) := (others => '0');     
+    signal BR_RST_i: std_logic := '0';
     
+    --Outputs
+    signal BR_Qa_o : std_logic_vector(7 downto 0) := (others => '0');
+    signal BR_Qb_B_o : std_logic_vector(7 downto 0) := (others => '0');
+
 begin
 
-    Label_LIDI: etage_pipeline PORT MAP (
-        A_i => LIDI_i(7 downto 0),
-        B_i => LIDI_i(23 downto 16),
-        C_i => LIDI_i(31 downto 24),
-        OP_i => LIDI_i(15 downto 8),
-        CLK => CLK_c,
-        A_o => LIDI_A_o,
-        B_o => LIDI_B_o,
-        C_o => LIDI_C_o,
-        OP_o => LIDI_OP_o
-    );
-    
-    Label_DIEX: etage_pipeline PORT MAP (
-        A_i => LIDI_A_o,
-        B_i => DIEX_B_i,
-        C_i => DIEX_C_i,
-        OP_i => LIDI_OP_o,
-        CLK => CLK_c,
-        A_o => DIEX_A_o,
-        B_o => DIEX_B_o,
-        C_o => DIEX_C_o,
-        OP_o => DIEX_OP_o
-    );
-    
-    Label_EXMEM: etage_pipeline PORT MAP (
-        A_i => DIEX_A_o,
-        B_i => EXMEM_B_i,
-        C_i => EXMEM_C_i,
-        OP_i => LIDI_OP_o,
-        CLK => CLK_c,
-        A_o => EXMEM_A_o,
-        B_o => EXMEM_B_o,
-        C_o => EXMEM_C_o,
-        OP_o => EXMEM_OP_o
-    );
-    
-    Label_MEMRE: etage_pipeline PORT MAP (
-        A_i => EXMEM_A_o,
-        B_i => MEMRE_B_i,
-        C_i => MEMRE_C_i,
-        OP_i => EXMEM_OP_o,
-        CLK => CLK_c,
-        A_o => MEMRE_A_o,
-        B_o => MEMRE_B_o,
-        C_o => MEMRE_C_o,
-        OP_o => MEMRE_OP_o
-    );
-    
-    Label_Banc_Instruction: banc_instructions PORT MAP (
-        ADDR => BAIN_ADDR_i,
-        CLK => CLK_c,
-        OUTD => LIDI_i
-    );
+--Addition 0x01
+--Multiplication 0x02
+--Soustraction 0x03
+--Division 0x04
+--Copie 0x05
+--Affectation 0x06
+--Chargement 0x07
+--Sauvegarde 0x08
 
-
--- Multiplexeur
-process
+-- Multiplexeurs
+process(CLK_c)
 begin
-    case LIDI_i is
-        when x"06" => DIEX_B_i <= LIDI_B_o;
-        
+  if rising_edge(CLK_c) then
+  
+    LIDI_A_o <= LIDI_i(7 downto 0);
+    LIDI_B_o <= LIDI_i(15 downto 0);
+    LIDI_C_o <= LIDI_i(23 downto 0);
+    LIDI_OP_o <= LIDI_i(31 downto 0);
+    
+    DIEX_A_o <= DIEX_A_i;
+    DIEX_B_o <= DIEX_B_i;
+    DIEX_C_o <= DIEX_C_i;
+    DIEX_OP_o <= DIEX_OP_i;
+
+    EXMEM_A_o <= EXMEM_A_i;
+    EXMEM_B_o <= EXMEM_B_i;
+    EXMEM_C_o <= EXMEM_C_i;
+    EXMEM_OP_o <= EXMEM_OP_i;
+
+    MEMRE_A_o <= MEMRE_A_i;
+    MEMRE_B_o <= MEMRE_B_i;
+    MEMRE_C_o <= MEMRE_C_i;
+    MEMRE_OP_o <= MEMRE_OP_i;
+    
+    case LIDI_OP_o is
+      when x"01" =>     
+      when x"02" =>
+      when x"03" =>
+      when x"04" =>
+      when x"05" =>
+      when x"06" =>
+        DIEX_A_i <= LIDI_A_o;
+        DIEX_B_i <= LIDI_B_o;
+        DIEX_OP_i <= LIDI_OP_o;
+      when others => null;
     end case;
+    case DIEX_OP_o is
+      when x"01" =>     
+      when x"02" =>
+      when x"03" =>
+      when x"04" =>
+      when x"05" =>
+      when x"06" =>
+        EXMEM_A_i <= DIEX_A_o;
+        EXMEM_B_i <= DIEX_B_o;
+        EXMEM_OP_i <= DIEX_OP_o;
+      when others => null;
+    end case;
+    case EXMEM_OP_o is
+      when x"01" =>     
+      when x"02" =>
+      when x"03" =>
+      when x"04" =>
+      when x"05" =>
+      when x"06" =>
+        MEMRE_A_i <= EXMEM_A_o;
+        MEMRE_B_i <= EXMEM_B_o;
+        MEMRE_OP_i <= EXMEM_OP_o;
+      when others => null;
+    end case;
+    case MEMRE_OP_o is
+      when x"01" =>     
+      when x"02" =>
+      when x"03" =>
+      when x"04" =>
+      when x"05" =>
+      when x"06" =>
+        BR_W_i <= EXMEM_A_o;
+        BR_Wb_i <= '1';
+        BR_DATA_i <= EXMEM_B_o;
+      when others => null;
+    end case;
+  end if;
 end process;
-end Structural;
+
+
