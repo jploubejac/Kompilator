5b4e0ab02d1ffc9338418d162fcb96a4fca18e8d
Clement Lacau <lacau@insa-toulouse.fr>
2025-05-20 11:54:10 +0200

    add: code c de demo

--- kompilator/sample_files/ex10.c
+++ kompilator/sample_files/ex10.c
@@ -1,14 +1,31 @@
 void main() {
-    int a = 0;
     int led_to_choose = 0;
-    while (a < 30) {
-        led_to_choose = led_to_choose * a + a;
+    int score = 0;
+    int timer = 60;
+    int button;
+    int timer_while=0;
+    int win;
+    while (timer > 1) {
+        led_to_choose = led_to_choose + timer;
         while (led_to_choose > 15) {
             led_to_choose = led_to_choose - 15;
         }
-        a = a +1;
-        // led status
-        // if low, turn it on, if high check switch value
+        int valeur_attendue = 1;
+        int i = led_to_choose;
+        while (i > 0) {
+            valeur_attendue = valeur_attendue * 2;
+            i = i - 1;
+        }
+        timer_while=0;
+        win =0;
+        while((timer_while < 5) && (win == 0)){
+            if (button == valeur_attendue) {
+                score = score + 1;
+                win=1;
+            }
+            timer_while = timer_while + 1;
+        }
+        timer = timer - 1;
     }
     int c = 0;
 }
